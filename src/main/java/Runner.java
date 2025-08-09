cc9ea248efc1e79ee560fc5451b53a58a872f497
sharmachait <chait8126@gmail.com>
2025-08-09 15:33:11 +0530

    testing

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,12 +1,15 @@
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Runner {
     public static void run(String filename) {
         String fileContents = "";
         try {
-            fileContents = Files.readString(Path.of(filename));
+            byte[] bytes = Files.readAllBytes(Path.of(filename));
+            fileContents = new String(bytes, Charset.defaultCharset());
             System.out.println(fileContents);
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
