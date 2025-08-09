57fef8bf80c5ac2d10b35d30c765c0d3c4cee6ea
sharmachait <chait8126@gmail.com>
2025-08-09 14:50:41 +0530

    Scanning: Empty file #ry8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,6 @@ import java.nio.file.Path;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
-
     if (args.length < 2) {
       System.err.println("Usage: ./your_program.sh tokenize <filename>");
       System.exit(1);
@@ -28,12 +25,10 @@ public class Main {
       System.exit(1);
     }
 
-    // Uncomment this block to pass the first stage
-    // 
-    // if (fileContents.length() > 0) {
-    //   throw new RuntimeException("Scanner not implemented");
-    // } else {
-    //   System.out.println("EOF  null"); // Placeholder, replace this line when implementing the scanner
-    // }
+     if (fileContents.length() > 0) {
+       throw new RuntimeException("Scanner not implemented");
+     } else {
+       System.out.println("EOF  null"); // Placeholder, replace this line when implementing the scanner
+     }
   }
 }
